f4d5def4ceda5e94d711b531b36b5509b88d1ecd
mallickrabin102-rgb <mallickrabin102@gmail.com>
2026-06-01 12:01:49 +0530

    Remove database credentials

--- DBconnection.java
+++ DBconnection.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 public class DBconnection {
     public static Connection getConnection() throws Exception{
         return DriverManager.getConnection("jdbc:mysql://localhost:3306/expensedb",
-            "root","12345"
+            "root","PASSWORD_REMOVED"
         );
     }
 }
